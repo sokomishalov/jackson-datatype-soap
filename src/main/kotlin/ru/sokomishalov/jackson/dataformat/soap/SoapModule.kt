0bb6269e444d0deb2e27e14439a9393d8cac2d0c
Mikhael Sokolov <mikhael.sokolov@mynd.co>
2022-04-12 17:38:37 +0300

    fix #37

--- src/main/kotlin/ru/sokomishalov/jackson/dataformat/soap/SoapModule.kt
+++ src/main/kotlin/ru/sokomishalov/jackson/dataformat/soap/SoapModule.kt
@@ -19,15 +19,15 @@
 
 package ru.sokomishalov.jackson.dataformat.soap
 
+import com.ctc.wstx.stax.WstxOutputFactory
 import com.fasterxml.jackson.databind.DeserializationFeature.*
+import com.fasterxml.jackson.databind.MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS
 import com.fasterxml.jackson.databind.MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature.FAIL_ON_EMPTY_BEANS
 import com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
-import com.fasterxml.jackson.databind.type.TypeFactory
 import com.fasterxml.jackson.dataformat.xml.XmlFactory
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule
-import org.codehaus.stax2.XMLOutputFactory2.P_AUTOMATIC_NS_PREFIX
 import ru.sokomishalov.jackson.dataformat.soap.SoapConstants.SOAP_11_ENVELOPE_NAMESPACE
 import ru.sokomishalov.jackson.dataformat.soap.deser.SoapEnvelopeDeserializers
 import ru.sokomishalov.jackson.dataformat.soap.ser.SoapEnvelopeSerializers
@@ -38,18 +38,19 @@ import ru.sokomishalov.jackson.dataformat.soap.ser.SoapEnvelopeSerializers
 open class SoapModule @JvmOverloads constructor(
     private val namespacePrefix: String = "ns",
     private val soapEnvelopeNamespace: String = SOAP_11_ENVELOPE_NAMESPACE
-) : JaxbAnnotationModule(SoapJaxbAnnotationIntrospector(TypeFactory.defaultInstance())) {
+) : JaxbAnnotationModule(SoapJaxbAnnotationIntrospector()) {
 
     override fun setupModule(context: SetupContext) {
         with(context) {
             addSerializers(SoapEnvelopeSerializers(soapEnvelopeNamespace))
             addDeserializers(SoapEnvelopeDeserializers())
             getOwner<ObjectMapper>().apply {
+                setSerializerProvider(AlwaysUseNamespacesXmlSerializerProvider())
                 enable(ACCEPT_SINGLE_VALUE_AS_ARRAY, ACCEPT_FLOAT_AS_INT)
-                enable(ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+                enable(ACCEPT_CASE_INSENSITIVE_PROPERTIES, ACCEPT_CASE_INSENSITIVE_ENUMS)
                 disable(FAIL_ON_EMPTY_BEANS, WRITE_DATES_AS_TIMESTAMPS)
-                disable(FAIL_ON_READING_DUP_TREE_KEY, FAIL_ON_UNKNOWN_PROPERTIES)
-                (factory as? XmlFactory)?.xmlOutputFactory?.setProperty(P_AUTOMATIC_NS_PREFIX, namespacePrefix)
+                disable(FAIL_ON_UNKNOWN_PROPERTIES, FAIL_ON_READING_DUP_TREE_KEY)
+                ((factory as? XmlFactory)?.xmlOutputFactory as? WstxOutputFactory)?.apply { config.automaticNsPrefix = namespacePrefix }
             }
             super.setupModule(this)
         }
